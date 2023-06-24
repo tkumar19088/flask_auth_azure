11f38258c1ce476716487bc7859139eeb075b7c1
Pavan Kumar T <73464747+tkumar19088@users.noreply.github.com>
2023-06-24 14:01:46 +0530

    adding sample flask content and templates

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask import render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return "Hello, World!"
+def countdown():
+    launchTime = datetime(2023, 6, 1)
+    currentTime = datetime.now()
+    diff = launchTime - currentTime
+    numberOfMilliseconds = int(diff.total_seconds() * 1000)
+
+    return render_template("countdown.html", time=numberOfMilliseconds)
