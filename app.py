9b373a87cd6570872b863615d78553c9604316ed
Pavan Kumar T <73464747+tkumar19088@users.noreply.github.com>
2023-07-05 20:15:18 +0530

    getting user_role from azure login

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
 from datetime import datetime
+from flask_dance.contrib.azure import make_azure_blueprint, azure
 
 app = Flask(__name__)
+
+# azure_bp = make_azure_blueprint()
+# app.register_blueprint(azure_bp, url_prefix="/login")
+
+
+# @app.route("/")
+# def index():
+#     if not azure.authorized:
+#         return redirect(url_for("azure.login"))
+#     return "Welcome to the Flask app!"
 
 
 @app.route("/")
 def countdown():
-    launchTime = datetime(2023, 6, 1)
+    userrole = azure.token["roles"]
+    launchTime = datetime(2023, 7, 30)
     currentTime = datetime.now()
     diff = launchTime - currentTime
     numberOfDays = diff.days
 
-    return render_template("countdown.html", time=numberOfDays)
+    return render_template("countdown.html", time=numberOfDays, role=userrole)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
