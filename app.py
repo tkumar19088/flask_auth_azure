7a41f09ce5cc1d697b6910063e6162a965429fc8
Pavan Kumar T <73464747+tkumar19088@users.noreply.github.com>
2023-07-05 20:22:04 +0530

    getting username and role from azure ad

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, redirect, url_for
 from datetime import datetime
 from flask_dance.contrib.azure import make_azure_blueprint, azure
+from flask import Flask, session
 
 app = Flask(__name__)
 
@@ -18,13 +19,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def countdown():
-    userrole = azure.token["roles"]
-    launchTime = datetime(2023, 7, 30)
-    currentTime = datetime.now()
-    diff = launchTime - currentTime
-    numberOfDays = diff.days
-
-    return render_template("countdown.html", time=numberOfDays, role=userrole)
+    if "user" in session:
+        user = session["user"]
+        # Read the user's role from the session
+        user_role = user.get("role")
+        launchTime = datetime(2023, 7, 30)
+        currentTime = datetime.now()
+        diff = launchTime - currentTime
+        numberOfDays = diff.days
+        return render_template(
+            "countdown.html",
+            time=numberOfDays,
+            username=user["name"],
+            userrole=user_role,
+        )
+    else:
+        return render_template(
+            "countdown.html",
+            time="#0#0",
+            username="UNAUTHORISED",
+            userrole="UNAUTHORISED",
+        )
 
 
 if __name__ == "__main__":
