62e7518bc0edf24dfa1d7f5ae2b3958bedb6bb13
Pavan Kumar T <73464747+tkumar19088@users.noreply.github.com>
2023-07-11 10:16:02 +0530

    base static without authentication

--- app.py
+++ app.py
@@ -4,117 +4,119 @@ from flask_session import Session
 from msal import ConfidentialClientApplication, SerializableTokenCache
 import requests
 import app_config
-from werkzeug.middleware.proxy_fix import ProxyFix
+
+# from werkzeug.middleware.proxy_fix import ProxyFix
 
 # Create the Flask app
 app = Flask(__name__)
-app.config.from_object(app_config)
+# app.config.from_object(app_config)
 Session(app)
 
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+# app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 
 @app.route("/")
 def index():
-    if "user" in session:
-        launchTime = datetime(2023, 7, 30)
-        currentTime = datetime.now()
-        diff = launchTime - currentTime
-        numberOfDays = diff.days
-        uname = session["user"].get("name")
-        urole = session["user"].get("roles")
-        return render_template(
-            "countdown.html", time=numberOfDays, uname=uname, urole=urole
-        )
-    else:
-        return redirect(url_for("login"))
-
-
-@app.route("/login")
-def login():
-    # Technically we could use empty list [] as scopes to do just sign in,
-    # here we choose to also collect end user consent upfront
-    session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE)
-    auth_url = session["flow"]["auth_uri"]
-    return redirect(auth_url)
-
-
-@app.route("/redirect")
-def authorized():
-    try:
-        cache = _load_cache()
-        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
-            session.get("flow", {}), request.args
-        )
-        if "error" in result:
-            return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
-        print(f'\n\n{session["user"]}\n\n')
-        _save_cache(cache)
-    except ValueError:  # Usually caused by CSRF
-        pass  # Simply ignore them
-    return redirect(url_for("index"))
-
-
-@app.route("/logout")
-def logout():
-    session.clear()  # Wipe out user and its token cache from session
-    return redirect(  # Also logout from your tenant's web session
-        app_config.AUTHORITY
-        + "/oauth2/v2.0/logout"
-        + "?post_logout_redirect_uri="
-        + url_for("index", _external=True)
+    # if "user" in session:
+    launchTime = datetime(2023, 7, 30)
+    currentTime = datetime.now()
+    diff = launchTime - currentTime
+    numberOfDays = diff.days
+    # uname = session["user"].get("name")
+    # urole = session["user"].get("roles")
+    return render_template(
+        "countdown.html", time=numberOfDays, uname="Pavan", urole="DirectRole"
     )
+    # else:
+    #     return redirect(url_for("login"))
 
 
-@app.route("/graphcall")
-def graphcall():
-    token = _get_token_from_cache(app_config.SCOPE)
-    if not token:
-        return redirect(url_for("login"))
-    graph_data = requests.get(  # Use token to call downstream service
-        app_config.ENDPOINT,
-        headers={"Authorization": "Bearer " + token["access_token"]},
-    ).json()
-    return render_template("display.html", result=graph_data)
+# @app.route("/login")
+# def login():
+#     # Technically we could use empty list [] as scopes to do just sign in,
+#     # here we choose to also collect end user consent upfront
+#     session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE)
+#     auth_url = session["flow"]["auth_uri"]
+#     return redirect(auth_url)
 
 
-def _load_cache():
-    cache = SerializableTokenCache()
-    if session.get("token_cache"):
-        cache.deserialize(session["token_cache"])
-    return cache
+# @app.route("/redirect")
+# def authorized():
+#     try:
+#         cache = _load_cache()
+#         result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
+#             session.get("flow", {}), request.args
+#         )
+#         if "error" in result:
+#             return render_template("auth_error.html", result=result)
+#         session["user"] = result.get("id_token_claims")
+#         print(f'\n\n{session["user"]}\n\n')
+#         _save_cache(cache)
+#     except ValueError:  # Usually caused by CSRF
+#         pass  # Simply ignore them
+#     return redirect(url_for("index"))
 
 
-def _save_cache(cache):
-    if cache.has_state_changed:
-        session["token_cache"] = cache.serialize()
+# @app.route("/logout")
+# def logout():
+#     session.clear()  # Wipe out user and its token cache from session
+#     return redirect(  # Also logout from your tenant's web session
+#         app_config.AUTHORITY
+#         + "/oauth2/v2.0/logout"
+#         + "?post_logout_redirect_uri="
+#         + url_for("index", _external=True)
+#     )
 
 
-def _build_msal_app(cache=None, authority=None):
-    return ConfidentialClientApplication(
-        app_config.CLIENT_ID,
-        authority=app_config.AUTHORITY,
-        client_credential=app_config.CLIENT_SECRET,
-        token_cache=cache,
-    )
+# @app.route("/graphcall")
+# def graphcall():
+#     token = _get_token_from_cache(app_config.SCOPE)
+#     if not token:
+#         return redirect(url_for("login"))
+#     graph_data = requests.get(  # Use token to call downstream service
+#         app_config.ENDPOINT,
+#         headers={"Authorization": "Bearer " + token["access_token"]},
+#     ).json()
+#     return render_template("display.html", result=graph_data)
 
 
-def _build_auth_code_flow(authority=None, scopes=None):
-    return _build_msal_app(authority=authority).initiate_auth_code_flow(
-        scopes or [], redirect_uri=url_for("authorized", _external=True)
-    )
+# def _load_cache():
+#     cache = SerializableTokenCache()
+#     if session.get("token_cache"):
+#         cache.deserialize(session["token_cache"])
+#     return cache
 
 
-def _get_token_from_cache(scope=None):
-    cache = _load_cache()  # This web app maintains one cache per session
-    cca = _build_msal_app(cache=cache)
-    accounts = cca.get_accounts()
-    if accounts:  # So all account(s) belong to the current signed-in user
-        result = cca.acquire_token_silent(scope, account=accounts[0])
-        _save_cache(cache)
-        return result
+# def _save_cache(cache):
+#     if cache.has_state_changed:
+#         session["token_cache"] = cache.serialize()
+
+
+# def _build_msal_app(cache=None, authority=None):
+#     return ConfidentialClientApplication(
+#         app_config.CLIENT_ID,
+#         authority=app_config.AUTHORITY,
+#         client_credential=app_config.CLIENT_SECRET,
+#         token_cache=cache,
+#     )
+
+
+# def _build_auth_code_flow(authority=None, scopes=None):
+#     return _build_msal_app(authority=authority).initiate_auth_code_flow(
+#         scopes or [], redirect_uri=url_for("authorized", _external=True)
+#     )
+
+
+# def _get_token_from_cache(scope=None):
+#     cache = _load_cache()  # This web app maintains one cache per session
+#     cca = _build_msal_app(cache=cache)
+#     accounts = cca.get_accounts()
+#     if accounts:  # So all account(s) belong to the current signed-in user
+#         result = cca.acquire_token_silent(scope, account=accounts[0])
+#         _save_cache(cache)
+#         return result
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port=5000)
+    # app.run(debug=True, host="localhost", port=5000)
+    app.run()
